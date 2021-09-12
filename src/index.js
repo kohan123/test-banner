93eb29f0a53d389bc86e28d8d2634a87061f6f90
Danil Kokhan <sportodavka@gmail.com>
2021-09-12 03:56:13 +0300

    feat: create section comments

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 const bannerTemplate = require("./templates/banner.handlebars");
 require("./styles/main.scss");
-const image1 = require("./assets/background.png");
-const image2 = require("./assets/group2.png");
+import image1 from "./assets/background.png";
+import image2 from "./assets/group2.png";
+import image3 from "./assets/bitmap.png";
+import image4 from "./assets/Star.png";
 
 document.addEventListener("DOMContentLoaded", function () {
   var div = document.createElement("div");
   div.innerHTML = bannerTemplate({
-    logoImg: image1,
-    close: image2,
+    image1,
+    image2,
+    image3,
+    image4,
   });
   document.body.appendChild(div);
 });
