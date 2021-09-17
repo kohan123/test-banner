22286c52588b47f905792ad2de7ce6d700bb75de
Danil Kokhan <sportodavka@gmail.com>
2021-09-17 04:41:52 +0300

    feat: positioning prices

--- src/scripts/price.js
+++ src/scripts/price.js
@@ -1,14 +1,14 @@
 export const registerClicks = () => {
-   document.querySelector(".btn").addEventListener("click", () => {
-      const selectedPriceElement = document.querySelector(".price.selected");
-      if (selectedPriceElement) {
-        window.open(selectedPriceElement.dataset.link, "_blank")
-      }
-   }) 
+  document.querySelector(".btn").addEventListener("click", () => {
+    const selectedPriceElement = document.querySelector(".price.selected");
+    if (selectedPriceElement) {
+      window.open(selectedPriceElement.dataset.link, "_blank");
+    }
+  });
   document.querySelector(".prices").addEventListener("click", (event) => {
     const priceElement = event.target.closest(".price");
     document.querySelectorAll(".price").forEach((element) => {
-        element.classList.remove("selected")
+      element.classList.remove("selected");
     });
     priceElement.classList.add("selected");
   });
