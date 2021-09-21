691848bae8053f2b2ad419567f858373c67517ab
Danil Kokhan <sportodavka@gmail.com>
2021-09-22 02:38:57 +0300

    feat: add graph

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ import image1 from "./assets/background.png";
 import image2 from "./assets/group2.png";
 import image3 from "./assets/bitmap.png";
 import image4 from "./assets/Star.png";
+import image5 from "./assets/bottom.png";
+import image6 from "./assets/top.png";
+import image7 from "./assets/group6.png";
 import { initSlider } from "./templates/scroll";
 import { registerClicks } from "./scripts/price";
 
@@ -16,6 +19,9 @@ document.addEventListener("DOMContentLoaded", function () {
     image2,
     image3,
     image4,
+    image5,
+    image6,
+    image7,
   });
   document.body.appendChild(div);
 
